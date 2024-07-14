1b6b71b1f3e6382a84494e539c79a39c3f468c85
2210631170002 <2210631170002@student.unsika.ac.id>
2024-07-14 13:47:05 +0000

    Build for popular location section

--- src/components/popular-locations/popular-locations.tsx
+++ src/components/popular-locations/popular-locations.tsx
@@ -4,12 +4,39 @@ import React from 'react'
 import Card from './card'
 import { useQuery } from '@tanstack/react-query'
 import { getPopularPlaces } from './service'
+import Delhi from '../../../public/img/delhi.jpg'
+import Dubai from '../../../public/img/dubai.jpg'
+import Berlin from '../../../public/img/berlin.jpg'
+import Paris from '../../../public/img/paris.jpg'
 
 const PopularLocations = () => {
-  const { data, isLoading } = useQuery({
-    queryFn: getPopularPlaces,
-    queryKey: ["popular-listings"]
-  })
+  // const { data, isLoading } = useQuery({
+  //   queryFn: getPopularPlaces,
+  //   queryKey: ["popular-listings"]
+  // })
+
+  const data = [
+    {
+      image: Delhi,
+      city: "Delhi",
+      numOfPlaces: 73
+    },
+    {
+      image: Berlin,
+      city: "Berlin",
+      numOfPlaces: 34
+    },
+    {
+      image: Paris,
+      city: "Paris",
+      numOfPlaces: 52
+    },
+    {
+      image: Dubai,
+      city: "Dubai",
+      numOfPlaces: 27
+    },
+  ]
 
   return (
     <div className="h-full w-full my-36">
