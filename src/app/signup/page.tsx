644bc905f3adb3f55c37e27e447547fb1fc8b2b6
anthrofax <afridhoikhsan@gmail.com>
2024-07-16 19:36:36 +0700

    Implement sign up functionality on sign up page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,51 +1,89 @@
-'use client'
+"use client";
+import Image from "next/image";
+import Dubai from "../../../public/img/dubai.jpg";
+import React from "react";
+import Input from "@/ui/Input";
+import Button from "@/ui/Button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { schema } from "./schema";
+import { useRouter } from "next/navigation";
+import AXIOS_API from "@/utils/axios-api";
+import { toast } from "react-hot-toast";
 
-import Image from "next/image"
-import Dubai from "../../../public/img/dubai.jpg"
-import Input from "@/ui/Input"
-import Button from "@/ui/Button"
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-function Signup() {
-    return (
-        <div className="relative h-screen w-full">
-        <div className="relative h-full w-full">
-          <Image
-            alt="Dubai Background Image"
-            src={Dubai}
-            className="brightness-50 h-full w-full object-cover"
+  const router = useRouter();
+  const onSubmit = async (data: any) => {
+    if (Object.keys(errors)?.length > 0) {
+      toast.error("Enter valid data");
+      return;
+    }
+console.log(data)
+    try {
+      await AXIOS_API.post("/register", data);
 
-          />
-          <div
-            className="h-[450px] w-[400px] bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg"
-          > 
-            <h2 className="text-center p-4 font-semibold text-slate-800 text-2xl border-b border-slate-500">
-              Create an account
-            </h2>
-            <form  className="mt-12 flex flex-col items-center w-full gap-8">
-              <Input
-                className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
-                type="text"
-                placeholder="John123"
-              />
-              <Input
-                className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
-                type="email"
-                placeholder="johndoe@gmail.com"
-              />
-              <Input
-                className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
-                type="password"
-                placeholder="********"
-              />
-              <Button
-                className="w-3/4 mt-12 mx-auto cursor-pointer rounded-lg py-2 px-6 text-xl text-white bg-blue-500 transition-all hover:bg-blue-600"
-                label="Submit"
-              />
-            </form>
-          </div>
+      toast.success("Success! Redirecting to login");
+
+      setTimeout(() => {
+        router.push("/login");
+      }, 2500);
+    } catch (error) {
+      console.log(error);
+      toast.error("Terdapat kesalahan saat mendaftarkan akun anda.")
+    }
+  };
+
+  return (
+    <div className="relative h-screen w-full">
+      <div className="relative h-full w-full">
+        <Image
+          alt="Dumai Background Image"
+          src={Dubai}
+          className="brightness-50 h-full w-full object-cover"
+        />
+        <div className="h-[450px] w-[400px] bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg">
+          <h2 className="text-center p-4 font-semibold text-slate-800 text-2xl border-b border-slate-500">
+            Create an account
+          </h2>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-12 flex flex-col items-center w-full gap-8"
+          >
+            <Input
+              className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
+              type="text"
+              placeholder="John123"
+              register={register("username")}
+            />
+            <Input
+              className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
+              type="email"
+              placeholder="johndoe@gmail.com"
+              register={register("email")}
+            />
+            <Input
+              className="w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
+              type="password"
+              placeholder="********"
+              register={register("password")}
+            />
+            <Button
+              className="w-3/4 mt-12 mx-auto cursor-pointer rounded-lg py-2 px-6 text-xl text-white bg-blue-500 transition-all hover:bg-blue-600"
+              label="Submit"
+            />
+          </form>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Signup
+export default SignUp;
