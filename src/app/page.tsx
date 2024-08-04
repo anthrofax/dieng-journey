111da59c76b6d52182a79aad40b016a753c1a0d7
NMAtech <nadindramaulanaaziz@gmail.com>
2024-08-04 22:02:00 +0700

    build section comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import PopularDestinations from "@/components/popular-destinations/popular-destinations";
 import HeroSlider from "@/components/hero/hero";
 import About from "@/components/about/about";
+import Comments from "@/components/comments/comments";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <PopularDestinations />
       {/* <BestHotels /> */}
       <About />
+      <Comments />
     </>
   );
 }
